aa49159aed24664f1e081c36c637b7f13a4cc750
Andrew James <bitbucket@andrew.tt9.co>
2017-06-30 21:23:41 +0000

    Updated macOS defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -9,10 +9,10 @@
 # Run ./set-defaults.sh and you'll be good to go.
 
 # Ask for the administrator password upfront
-sudo -v
+#sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
@@ -68,10 +68,10 @@ defaults write com.apple.helpviewer DevMode -bool true
 
 # Reveal IP address, hostname, OS version, etc. when clicking the clock
 # in the login window
-sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+#defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
 # Restart automatically if the computer freezes
-systemsetup -setrestartfreeze on
+#systemsetup -setrestartfreeze on
 
 # Never go into computer sleep mode
 #systemsetup -setcomputersleep Off > /dev/null
@@ -127,7 +127,10 @@ defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
 defaults write NSGlobalDomain AppleMetricUnits -bool true
 
 # Set the timezone; see `systemsetup -listtimezones` for other values
-systemsetup -settimezone "Europe/London" > /dev/null
+sudo systemsetup -settimezone "Europe/London" > /dev/null
+
+#Save to disk by default (not iCloud):
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # Disable auto-correct
 #defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
@@ -333,9 +336,8 @@ defaults write com.apple.dock wvous-bl-modifier -int 0
 ###############################################################################
 
 #Prevent most recently used space reordering
-defaults write com.apple.dock mru-spaces  -int 0
+defaults write com.apple.dock mru-spaces  -bool false
 
-"mru-spaces" = 0;
 ###############################################################################
 # Safari & WebKit                                                             #
 ###############################################################################
