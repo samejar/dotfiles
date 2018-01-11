a0aaf265a0f0eec2ff4e89aaff8aa0742d3c44e5
Andrew James <bitbucket@andrew.tt9.co>
2018-01-11 22:27:18 +0000

    Removed yarn and added debug

--- node/install.sh
+++ node/install.sh
@@ -3,12 +3,14 @@ then
   sudo npm install spoof -g
 fi
 
+echo "Installing grunt-cli"
 npm install grunt-cli -g
+echo "Installing npm-check-updates"
 npm install npm-check-updates -g
-npm install yarn -g
 
+echo "Installing typescript"
 npm install typescript -g
-
+echo "Installing serverless"
 npm install serverless -g
-
+echo "Installing nativefier"
 npm install nativefier -g
