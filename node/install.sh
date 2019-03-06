c031e11776370c2ca2b09873d9f2c6eb683fc7a4
Andrew James <bitbucket@andrew.tt9.co>
2019-03-06 11:35:59 +0000

    Updated npm installer

--- node/install.sh
+++ node/install.sh
@@ -3,17 +3,20 @@ then
   sudo npm install spoof -g
 fi
 
-echo "Installing grunt-cli"
-npm install grunt-cli -g
+
 echo "Installing npm-check-updates"
 npm install npm-check-updates -g
+echo "Installing npx"
+npm install npx -g
 
-echo "Installing typescript"
-npm install typescript -g
-echo "Installing serverless"
-npm install serverless -g
+echo "Installing bookmarklet"
+npm install bookmarklet -g
+
 echo "Installing nativefier"
 npm install nativefier -g
+echo "Installing typescript"
+npm install typescript -g
+
 echo "Installing API2HTML - OpeanAPI Doc Generator"
 npm install api2html -g
 echo "Installing redoc-cli - OpeanAPI Doc Generator"
@@ -21,11 +24,14 @@ npm install redoc-cli -g
 
 echo "Installing Cypress"
 npm install cypress -g
-echo "Installing npx"
-npm install npx -g
+
 echo "Installing snyk"
 npm install -g snyk
 
+echo "Installing serverless"
+npm install serverless -g
 
+echo "Installing grunt-cli"
+npm install grunt-cli -g
 echo "Installing Parcel"
 yarn global add parcel-bundler
