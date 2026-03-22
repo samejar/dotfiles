a0cf6073670716bc1c90167c8bc4b144921e7158
Andrew James <2149327+samejar@users.noreply.github.com>
2026-03-22 22:12:00 +0000

    remove project-specific development scripts

--- development/setup-keytree.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/development/keytree
-cd ~/development/keytree
-
-git clone ssh://git-keytree/jlr/jlr-ecommerce.git jlr-ecommerce
-git clone ssh://git-keytree/jlr/jlr-ecommerce-ui.git jlr-ecommerce-ui
-
-
--- development/setup-vorrad.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/development/vorrad
-
-cd ~/development/vorrad
-
-git clone git-tt9:tt9/vorrad.git vorrad-server
